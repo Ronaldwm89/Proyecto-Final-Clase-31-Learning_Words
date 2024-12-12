e7c20c3ebce41535f583ef0fded68c45e9ae64c3
Ronald <ronaldwmm1989@gmail.com>
2024-12-11 21:41:14 -0300

    Creatings randoms words in the cards

--- class_buttons.py
+++ class_buttons.py
@@ -2,7 +2,7 @@ from tkinter import Button
 
 
 class Botones(Button):
-    def __init__(self, img, cols, rows, hks):
+    def __init__(self, img, cols, rows, hks, name):
         super().__init__()
-        self.config(image=img, highlightthickness=hks)
+        self.config(image=img, highlightthickness=hks, command=name)
         self.grid(column=cols, row=rows)
--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from tkinter import *
 from class_buttons import Botones
+import pandas
+import random
+
+data = pandas.read_csv(r"data\data_words.csv")
+
+def word_random():
+    word_english = random.choice(data["English"])
+    word_spanish = data[data.English == word_english]
+    
+    canvas_bg.delete("word")
+    canvas_bg.create_text(400,263, text=word_english, font=FONT_UP, tags="word")
 
 #Constantes Fuentes y Background
 BG_COLOR = "#B1DDC6"
 FONT_LOW = ("Ariel", 40, "italic")
-FONT_UP = ("Ariel", 60, "italic")
+FONT_UP = ("Ariel", 60, "bold")
 
 #Creacion del root
 root = Tk()
@@ -19,18 +30,17 @@ wrong_img = PhotoImage(file=r"images\wrong.png")
 img_back = PhotoImage(file=r"images\card_back.png")
 img_front = PhotoImage(file=r"images\card_front.png")
 
-#Creacion del canvas, labels, images and configurations
+#Creacion del canvas, textos, images and configurations
 canvas_bg = Canvas(width=800, height=526, highlightthickness=0, bg=BG_COLOR)
 canvas_img_front = canvas_bg.create_image(400,263, image=img_front)
 #canvas_img_back = canvas_bg.create_image(400,263, image=img_back)
 language = canvas_bg.create_text(400,150, text="English", font=FONT_LOW)
-words = canvas_bg.create_text(400,263, text="Moorning", font=FONT_UP)
 canvas_bg.grid(column=0, row=0, columnspan=2)
 
 
 #Botones Creados desde la class_buttons
-wrong_button = Botones(wrong_img, 0,1,0)
-right_button = Botones(right_img, 1,1,0)
+wrong_button = Botones(wrong_img, 0,1,0, word_random)
+right_button = Botones(right_img, 1,1,0, word_random)
 
 
 
